f2b7f4ca18927cfe9ec62764552d31b78a47fb38
joe4260 <joe426097@gmail.com>
2023-03-19 15:10:37 +0900

    add preprocessing

--- Transfer_Learning.py
+++ Transfer_Learning.py
@@ -15,3 +15,26 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 
 import matplotlib.pyplot as plt
+
+#이미지 데이터 전처리 방법 정의
+data_path = '../chap05/data/catanddog/train'
+
+transform = transforms.Compose(
+  [
+    transforms.Resize([256, 256]),
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor()
+  ])
+train_dataset = torchvision.datasets.ImageFolder(
+  data_path,
+  transform = transform
+)
+train_loader= torch.utils.data.DataLoader(
+  train_dataset,
+  batch_size = 32,
+  num_workers = 8,
+  shuffle = True
+)
+
+print(len(train_dataset))
